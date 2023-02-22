2a33b3f7d134ff8dbddce3cf71474fee8f0b634f
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-22 16:56:42 +0530

    default navigation removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router';
+
 import './App.css';
 import ResponsiveDrawer from './components/Navbar/Navbar';
 function App() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    navigate('/home')
-  }, [])
   return (
     <div className="App">
       <ResponsiveDrawer />
