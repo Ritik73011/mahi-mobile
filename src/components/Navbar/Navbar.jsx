b981169e3697f776d6eba23027d133461141d7fc
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-23 14:01:52 +0530

    signup page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,6 +22,7 @@ import AppbarRight from "./AppbarRight";
 import Login from "../../pages/Login";
 import Profile from "../../pages/Profile";
 import { boxShadowsObj } from "../../utils/color";
+import Signup from "../../pages/Signup";
 const drawerWidth = 220;
 
 function ResponsiveDrawer(props) {
@@ -164,6 +165,7 @@ function ResponsiveDrawer(props) {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
         {/*ROUTES COMPONENTS*/}
       </Box>
