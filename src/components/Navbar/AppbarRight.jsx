9ea08af4070f83ca41d7c3d5837847c29a38d123
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-23 12:34:37 +0530

    click event added on icon

--- src/components/Navbar/AppbarRight.jsx
+++ src/components/Navbar/AppbarRight.jsx
@@ -2,8 +2,13 @@ import { Box, Typography } from "@mui/material";
 import LoginIcon from "@mui/icons-material/Login";
 import { useState } from "react";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { useNavigate } from "react-router";
 const AppbarRight = () => {
   const [login] = useState(false);
+  const navigate = useNavigate();
+  const handleClick = (path) => {
+    navigate(path);
+  };
   return (
     <Box
       sx={{
@@ -24,9 +29,17 @@ const AppbarRight = () => {
           : window.location.pathname.split("/")}
       </Typography>
       {login ? (
-        <AccountCircleIcon fontSize="large" sx={{ cursor: "pointer" }} />
+        <AccountCircleIcon
+          fontSize="large"
+          sx={{ cursor: "pointer" }}
+          onClick={() => handleClick("/profile")}
+        />
       ) : (
-        <LoginIcon fontSize="large" sx={{ cursor: "pointer" }} />
+        <LoginIcon
+          fontSize="large"
+          sx={{ cursor: "pointer" }}
+          onClick={() => handleClick("/login")}
+        />
       )}
     </Box>
   );
