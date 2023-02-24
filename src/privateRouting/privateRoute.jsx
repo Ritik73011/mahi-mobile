2e2626b96de396ab8922031c1f6e35fb1d9d4268
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-24 14:46:45 +0530

    using context api

--- src/privateRouting/privateRoute.jsx
+++ src/privateRouting/privateRoute.jsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useContext } from "react";
 import { Navigate } from "react-router-dom";
+import LoginContext from "../context/LoginContext";
 const PrivateRoute = ({ children }) => {
-  const [login] = useState(false);
-  return login ? <Navigate to={"/profile"} /> : children;
+  const { login } = useContext(LoginContext);
+  const route = window.location.pathname.split("/")[1];
+  if (route === "login") return login ? <Navigate to={"/profile"} /> : children;
+  return login ? children : <Navigate to={"/login"} />;
 };
 export default PrivateRoute;
