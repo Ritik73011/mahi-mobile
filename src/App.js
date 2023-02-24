af63fef97baf85034d62e104b8b4dc7cc21917c6
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-24 14:48:08 +0530

    wrapping application inside context provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import LoginContext from './context/LoginContext'
 import './App.css';
 import ResponsiveDrawer from './components/Navbar/Navbar';
+import { useState } from 'react';
 function App() {
+  const [login, setLogin] = useState(false);
+  const setLoginStatus = (val) => {
+    setLogin(val);
+  }
   return (
     <div className="App">
-      <ResponsiveDrawer />
+      <LoginContext.Provider value={{ login, setLoginStatus }}>
+        <ResponsiveDrawer />
+      </LoginContext.Provider>
     </div>
   );
 }
