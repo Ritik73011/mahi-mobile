2c99455c1ffd4a97fff913815cad35de1e4b36af
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-26 18:50:55 +0530

    user status checking

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import LoginContext from './context/LoginContext'
 import './App.css';
 import ResponsiveDrawer from './components/Navbar/Navbar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 function App() {
   const [login, setLogin] = useState(false);
+
+  const checkLogin = () => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLogin(true);
+      }
+    });
+  }
+
   const setLoginStatus = (val) => {
     setLogin(val);
   }
+
+  useEffect(() => {
+    checkLogin();
+  }, [login])
   return (
     <div className="App">
       <LoginContext.Provider value={{ login, setLoginStatus }}>
