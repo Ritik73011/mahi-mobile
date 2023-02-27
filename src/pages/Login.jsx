0a639b5d3436413658139090b8883682ad4ca5b4
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-27 14:09:32 +0530

    direct sign in using google

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../private";
 import ForgetPass from "../components/Popups/ForgetPass";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const Login = () => {
   const [data, setData] = useState({});
@@ -38,6 +39,28 @@ const Login = () => {
   const Navigate = useNavigate();
   const registerClick = () => {
     Navigate("/signup");
+  };
+
+  const googleSignup = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        //const credential = GoogleAuthProvider.credentialFromResult(result);
+        //const token = credential.accessToken;
+        // The signed-in user info.
+        //const user = result.user;
+        //const email = user.email;
+        //const name = user.displayName;
+        //const uid = user.uid;
+
+        alert("signup successfully...");
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
   };
 
   return (
@@ -109,6 +132,7 @@ const Login = () => {
               cursor: "pointer",
               marginTop: "8px",
             }}
+            onClick={googleSignup}
           />
         </Box>
         <Typography fontSize="14px" sx={{ marginTop: "10px" }}>
